181dcb03988710f3463da6b28b46d5729fcb1c30
Diego Guerra <diego.guerra.suarez@gmail.com>
2018-01-20 00:10:35 +0100

    log decorator using method missing

---
+++ log_decorator.rb
@@ -0,0 +1,22 @@
+# Using method_missing allows us to intercept any method call. Here we use that
+# to decorate every method call to a given object.
+
+class LogDecorator
+
+  def initialize(decorated)
+    @decorated = decorated
+  end
+
+  def method_missing(method_name, *args)
+    puts "~~> #{@decorated} received #{method_name} with (#{args.inspect})"
+    @decorated.send(method_name, *args)
+  end
+
+end
+
+str = LogDecorator.new("Hey ho let's go")
+
+# This will print:
+# ~~> Hey ho let's go received length with ([])
+# 15
+puts str.length
